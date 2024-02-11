3680c729de48119a15c5646b48487671af2ecc03
Mohammad Fiaz Rahman Nafee <faiazrahman70@gmail.com>
2024-02-11 13:04:33 +0600

    added image slider with badge

--- src/screens/Details.jsx
+++ src/screens/Details.jsx
@@ -1,121 +1,113 @@
-import {View, Text, StyleSheet, StatusBar, Image} from 'react-native'
-import React from 'react'
+import React, {useState, useRef} from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Image,
+  ScrollView,
+} from 'react-native'
 import {Colors, Dim} from '../constants/theme'
 
 import {SafeAreaView} from 'react-native-safe-area-context'
-import {CustomHeader} from '../components'
-
-import Entypo from 'react-native-vector-icons/Entypo'
+import {CustomHeader, ImageSliderItem} from '../components'
 
 export default function Details() {
+  const [fav, setFav] = useState(true)
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const handleIndex = ({nativeEvent}) => {
+    const offset = nativeEvent.contentOffset
+    // console.log(Math.floor(offset.x / Dim.width))
+
+    const index = Math.floor(offset.x / Dim.width)
+    setActiveIndex(index)
+  }
+
+  const badgeArr = [1, 2, 3]
+
   return (
     <SafeAreaView style={styles.details}>
       <StatusBar
         barStyle={'dark-content'}
-        backgroundColor={'#fff'}
+        // backgroundColor={'#fff'}
         translucent
       />
-      <CustomHeader title={'Base Camp Tent'} />
+      <CustomHeader
+        title={'Base Camp Tent'}
+        onPress={() => {
+          setFav(prev => !prev)
+        }}
+        fav={fav}
+      />
 
-      <View style={styles.imageContainer}>
-        <Image
-          source={require('../../assets/images/tv.jpg')}
-          style={{
-            height: '90%',
-            width: '90%',
-            borderRadius: 10,
+      {/* Image Container */}
+      <View>
+        <ScrollView
+          horizontal
+          contentContainerStyle={{
+            // backgroundColor: 'red',
+            height: Dim.height * 0.31,
           }}
-          resizeMode="contain"
-        />
+          snapToAlignment="center"
+          decelerationRate={0}
+          disableIntervalMomentum={true}
+          snapToInterval={Dim.width}
+          showsHorizontalScrollIndicator={false}
+          onScroll={handleIndex}>
+          {badgeArr.map((item, ind) => {
+            return <ImageSliderItem />
+          })}
+        </ScrollView>
+      </View>
 
-        <View style={styles.distanceC}>
-          <Entypo
-            name="location-pin"
-            size={17}
-            color={'#fff'}
-            style={{
-              marginRight: 5,
-            }}
-          />
-          <Text style={styles.distanceText}>3.2 km</Text>
-        </View>
-
-        <View
-          style={[
-            styles.distanceC,
-            {
-              left: 100,
-              backgroundColor: Colors.green,
-            },
-          ]}>
-          <Text style={styles.distanceText}>Available</Text>
-        </View>
-
-        <View style={styles.starC}>
-          <Image
-            source={require('../../assets/images/star.png')}
-            style={{
-              height: 17,
-              width: 17,
-              tintColor: Colors.primary,
-            }}
-          />
-          <Text style={styles.starText}>4.9</Text>
-        </View>
+      {/* slider thumb */}
+      {/* {backgroundColor: Colors.lightPrimaryOpacity} */}
+      <View style={styles.thumbContainer}>
+        {badgeArr.map((item, ind) => {
+          return (
+            <View
+              key={ind}
+              style={[
+                styles.thumb,
+                {
+                  backgroundColor:
+                    ind === activeIndex
+                      ? Colors.primary
+                      : Colors.lightPrimaryOpacity,
+                },
+              ]}
+            />
+          )
+        })}
       </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  starC: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    bottom: 12.5,
-    left: 165,
+  thumb: {
+    height: 8,
+    width: 8,
+    backgroundColor: Colors.primary,
+    borderRadius: 10,
   },
-  starText: {
-    fontSize: 11,
-    fontFamily: 'Roboto-Bold',
-    color: Colors.primary,
-    marginBottom: 2,
-    marginLeft: 2,
+  thumbContainer: {
+    height: Dim.height * 0.05,
+    width: Dim.width * 0.15,
+    // backgroundColor: 'red',
+    alignSelf: 'center',
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 7,
+    paddingRight: 7,
   },
   details: {
     flex: 1,
     // height: Dim.height,
     width: Dim.width,
     backgroundColor: '#fff',
-  },
-  distanceC: {
-    height: 23,
-    paddingLeft: 5,
-    paddingRight: 5,
-    backgroundColor: Colors.primary,
-    position: 'absolute',
-    bottom: 10,
-    left: 25,
-    flexDirection: 'row',
-    borderRadius: 6,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  distanceText: {
-    fontSize: 11,
-    fontFamily: 'Roboto-Medium',
-    color: '#fff',
-  },
-  imageContainer: {
-    height: Dim.height * 0.3,
-    width: Dim.width * 0.8,
-    alignSelf: 'center',
-    backgroundColor: Colors.cartItemBg,
-    borderRadius: 10,
-    overflow: 'hidden',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'relative',
   },
 })
