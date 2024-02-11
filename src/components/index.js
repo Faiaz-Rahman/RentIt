b99d969e5fc67148fc3a07707df92965f80ce4c7
Mohammad Fiaz Rahman Nafee <faiazrahman70@gmail.com>
2024-02-11 19:21:31 +0600

    details screen added (90%)

--- src/components/index.js
+++ src/components/index.js
@@ -4,8 +4,12 @@ import Header from './Header'
 import AvailableItem from './AvailableItem'
 import CustomHeader from './CustomHeader'
 import ImageSliderItem from './ImageSliderItem'
+import Button from './Button'
+import PricingContainer from './PricingContainer'
 
 export {
+  PricingContainer,
+  Button,
   CartItem,
   ListItem,
   ImageSliderItem,
