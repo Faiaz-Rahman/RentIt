15f878db694495e341575db1da2b6ab1b0db4c72
Faiaz-Rahman <faiazrahman70@gmail.com>
2024-02-13 00:20:01 +0600

    customized the Custom Button Component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,22 +2,30 @@ import React, {useState} from 'react'
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 import {Colors, Dim} from '../constants/theme'
 
-export default function Button({title, extraStyles, onPress}) {
-  return (
-    <TouchableOpacity style={[styles.button, extraStyles]} onPress={onPress}>
-      <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
-  )
+export default function Button({title, extraStyles, onPress, children}) {
+  if (!children) {
+    return (
+      <TouchableOpacity style={[styles.button, extraStyles]} onPress={onPress}>
+        <Text style={styles.title}>{title}</Text>
+      </TouchableOpacity>
+    )
+  } else {
+    return (
+      <TouchableOpacity style={[styles.button, extraStyles]} onPress={onPress}>
+        {children}
+      </TouchableOpacity>
+    )
+  }
 }
 const styles = StyleSheet.create({
   button: {
-    height: Dim.height * 0.1,
+    height: Dim.height * 0.08,
     width: Dim.width * 0.85,
     backgroundColor: Colors.primary,
     alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 25,
+    borderRadius: 15,
   },
   title: {
     fontSize: 15,
