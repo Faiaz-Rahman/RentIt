5f0e80c92cc4edbe509effb5bf9d0aea7803f26c
Faiaz-Rahman <faiazrahman70@gmail.com>
2024-02-29 00:41:02 +0600

    messages screen layout created ...

--- src/screens/Messages.jsx
+++ src/screens/Messages.jsx
@@ -1,16 +1,72 @@
-import {View, Text, StyleSheet} from 'react-native'
 import React from 'react'
+import {View, Text, StyleSheet, StatusBar, FlatList} from 'react-native'
+
+import {SafeAreaView} from 'react-native-safe-area-context'
+
+import {AvailableItem, Header} from '../components'
+import {Colors, Dim} from '../constants/theme'
 
 export default function Messages() {
+  const favArr = Array.from({length: 10}, (v, i) => i + 1)
   return (
-    <View style={styles.messages}>
-      <Text>Messages</Text>
-    </View>
+    <SafeAreaView
+      style={styles.messages}
+      initialMetrics={{
+        insets: {
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+        },
+      }}>
+      <StatusBar
+        translucent
+        backgroundColor={'transparent'}
+        barStyle={'light-content'}
+      />
+
+      <Header />
+
+      <View style={styles.listContainer}>
+        <FlatList
+          data={favArr}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingTop: Dim.height * 0.03,
+            paddingBottom: Dim.height * 0.1,
+          }}
+          initialNumToRender={8}
+          renderItem={({item, index}) => {
+            return (
+              <AvailableItem
+                key={index}
+                extraStyle={{
+                  marginBottom: 10,
+                }}
+                differentColors={true}
+                index={index}
+              />
+            )
+          }}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   messages: {
     flex: 1,
+    backgroundColor: Colors.homeBg,
+    position: 'relative',
+  },
+  listContainer: {
+    position: 'absolute',
+    top: Dim.height * 0.15,
+    height: Dim.height * 0.85,
+    width: Dim.width,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
   },
 })
