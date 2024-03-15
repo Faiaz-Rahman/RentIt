e55179b50b984d4ca3aace5354e5666505c5729e
Faiaz-Rahman <faiazrahman70@gmail.com>
2024-03-15 19:06:04 +0600

    adding new screen UI

--- src/screens/AddNew.jsx
+++ src/screens/AddNew.jsx
@@ -1,16 +1,36 @@
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, StatusBar} from 'react-native'
 import React from 'react'
+import {SafeAreaView} from 'react-native-safe-area-context'
+import {Colors} from '../constants/theme'
+import {Header} from '../components'
 
 export default function AddNew() {
   return (
-    <View style={styles.addNew}>
-      <Text>AddNew</Text>
-    </View>
+    <SafeAreaView
+      style={styles.addNew}
+      initialMetrics={{
+        insets: {
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+        },
+      }}>
+      <StatusBar
+        translucent
+        backgroundColor={'transparent'}
+        barStyle={'light-content'}
+      />
+
+      <Header />
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   addNew: {
     flex: 1,
+    backgroundColor: Colors.homeBg,
+    position: 'relative',
   },
 })
